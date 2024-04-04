2da6cd397a2a384d58a85fd32584994a73bc0885
baronwoy <woybaron@gmail.com>
2024-04-04 23:59:33 +0100

    Made main screen where game is played, updated ui

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -20,5 +20,20 @@ namespace Tetris
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_KeyDown_1(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
